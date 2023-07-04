13ff559de8eb1b9a000992975bf9d6fbfc430d51
Michael Joy <kodiakchaos@gmail.com>
2023-07-03 20:42:11 -0400

    wip exercise set input design

--- src/components/fields/FieldSets.vue
+++ src/components/fields/FieldSets.vue
@@ -16,7 +16,7 @@ useMultipleSetsWatcher(updateMultipleSets)
 
 const currentExerciseInputs: Ref<ExerciseInput[]> = ref([])
 const hasMultipleSets: Ref<boolean> = ref(false)
-const setCounter: Ref<number> = ref(0)
+const setIndexes: Ref<null[]> = ref([])
 
 function updateMultipleSets(multipleSets: boolean) {
   hasMultipleSets.value = multipleSets
@@ -41,27 +41,9 @@ function updateData(inputs: ExerciseInput[]) {
 function initSets(selectedInput: ExerciseInput, field: AnyField) {
   if (currentExerciseInputs.value.includes(selectedInput)) {
     actionStore.record[field] = actionStore.record?.[field] ?? [0] // Empty sets array
-    setCounter.value = actionStore.record[field].length
+    setIndexes.value = Array(actionStore.record[field].length).fill(null)
   } else {
     actionStore.record[field] = null // Nulling out other fields
-  }
-}
-
-function addInputSets(selectedInput: ExerciseInput, field: AnyField) {
-  if (currentExerciseInputs.value.includes(selectedInput) && hasMultipleSets.value) {
-    if (actionStore.record?.[field] && actionStore.record?.[field].length < Limit.MAX_SETS) {
-      actionStore.record[field].push(0) // Add default value
-      setCounter.value = actionStore.record[field].length
-    }
-  }
-}
-
-function removeInputSet(selectedInput: ExerciseInput, field: AnyField) {
-  if (currentExerciseInputs.value.includes(selectedInput) && hasMultipleSets.value) {
-    if (actionStore.record?.[field] && actionStore.record?.[field].length > 1) {
-      actionStore.record[field].pop() // Remove last value
-      setCounter.value = actionStore.record[field].length
-    }
   }
 }
 
@@ -87,6 +69,24 @@ function removeSet() {
   removeInputSet(exerciseInputs.Values['Resistence'], allFields.Values.resistence)
 }
 
+function addInputSets(selectedInput: ExerciseInput, field: AnyField) {
+  if (currentExerciseInputs.value.includes(selectedInput) && hasMultipleSets.value) {
+    if (actionStore.record?.[field] && actionStore.record?.[field].length < Limit.MAX_SETS) {
+      actionStore.record[field].push(0) // Add default value
+      setIndexes.value = Array(actionStore.record[field].length).fill(null)
+    }
+  }
+}
+
+function removeInputSet(selectedInput: ExerciseInput, field: AnyField) {
+  if (currentExerciseInputs.value.includes(selectedInput) && hasMultipleSets.value) {
+    if (actionStore.record?.[field] && actionStore.record?.[field].length > 1) {
+      actionStore.record[field].pop() // Remove last value
+      setIndexes.value = Array(actionStore.record[field].length).fill(null)
+    }
+  }
+}
+
 function inspectFormat(val: number[]) {
   return val?.join(', ') || '-'
 }
@@ -96,55 +96,179 @@ function inspectFormat(val: number[]) {
   <div class="text-weight-bold text-body1">Exercise Sets</div>
 
   <div v-if="inspecting" class="q-ml-sm">
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values.Reps)">
       {{ exerciseInputs.Values.Reps }}:
       {{ inspectFormat(actionStore.record?.reps) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values['Weight (lbs)'])">
       {{ exerciseInputs.Values['Weight (lbs)'] }}:
       {{ inspectFormat(actionStore.record?.weightLbs) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values['Distance (miles)'])">
       {{ exerciseInputs.Values['Distance (miles)'] }}:
       {{ inspectFormat(actionStore.record?.distanceMiles) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values['Duration (minutes)'])">
       {{ exerciseInputs.Values['Duration (minutes)'] }}:
       {{ inspectFormat(actionStore.record?.durationMinutes) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values.Watts)">
       {{ exerciseInputs.Values.Watts }}:
       {{ inspectFormat(actionStore.record?.watts) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values['Speed (mph)'])">
       {{ exerciseInputs.Values['Speed (mph)'] }}:
       {{ inspectFormat(actionStore.record?.speedMph) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values['Calories Burned'])">
       {{ exerciseInputs.Values['Calories Burned'] }}:
       {{ inspectFormat(actionStore.record?.calories) }}
     </li>
-    <li>
+    <li v-if="currentExerciseInputs.includes(exerciseInputs.Values.Resistence)">
       {{ exerciseInputs.Values.Resistence }}:
       {{ inspectFormat(actionStore.record?.resistence) }}
     </li>
   </div>
 
-  <!-- TODO - Compare template to v16 version to see how you could design this -->
   <div v-else>
-    <QBtn
-      :disable="!hasMultipleSets || setCounter === Limit.MAX_SETS"
-      round
-      color="positive"
-      :icon="Icon.ADD"
-      @click="addSet()"
-    />
-    <QBtn
-      :disable="!hasMultipleSets || setCounter === 1"
-      round
-      color="negative"
-      :icon="Icon.REMOVE"
-      @click="removeSet()"
-    />
+    <div class="row q-mb-sm">
+      <div class="col">
+        Sets organize the inputs you choose for this exercise into numbered groups that also display
+        how you previously performed. Multiple sets must be on to use additional sets.
+      </div>
+
+      <div v-if="hasMultipleSets" class="column reverse">
+        <div>
+          <QBtn
+            :disable="setIndexes.length === Limit.MAX_SETS"
+            color="positive"
+            class="q-ml-sm"
+            round
+            :icon="Icon.ADD"
+            @click="addSet()"
+          />
+          <QBtn
+            :disable="setIndexes.length === 1"
+            color="negative"
+            class="q-ml-sm"
+            round
+            :icon="Icon.REMOVE"
+            @click="removeSet()"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div v-for="(_, index) in setIndexes" :key="index" class="row q-mb-md">
+      <QBadge
+        outline
+        v-if="hasMultipleSets"
+        size="lg"
+        color="primary"
+        class="text-bold text-body1 q-mr-sm"
+      >
+        {{ index + 1 }}
+      </QBadge>
+      <div class="col">
+        <div class="row q-mt-xs">
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values.Reps)"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.reps[index]"
+            :label="exerciseInputs.Values.Reps"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values['Weight (lbs)'])"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.weightLbs[index]"
+            :label="exerciseInputs.Values['Weight (lbs)']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values['Distance (miles)'])"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.distanceMiles[index]"
+            :label="exerciseInputs.Values['Distance (miles)']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values['Duration (minutes)'])"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.durationMinutes[index]"
+            :label="exerciseInputs.Values['Duration (minutes)']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values.Watts)"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.watts[index]"
+            :label="exerciseInputs.Values['Watts']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values['Speed (mph)'])"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.speedMph[index]"
+            :label="exerciseInputs.Values['Speed (mph)']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values['Calories Burned'])"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.calories[index]"
+            :label="exerciseInputs.Values['Calories Burned']"
+          />
+          <QInput
+            v-if="currentExerciseInputs.includes(exerciseInputs.Values.Resistence)"
+            stack-label
+            class="col-6 q-mb-xs"
+            type="number"
+            filled
+            square
+            dense
+            hint="135 (5, 5, -10, 5, 5)"
+            v-model.number="actionStore.record.resistence[index]"
+            :label="exerciseInputs.Values['Resistence']"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
